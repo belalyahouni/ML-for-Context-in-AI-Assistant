357a94defe325978d56ba80614dfad0334ff6dac
BelalYahouni <belalyahouni@gmail.com>
2025-10-13 09:36:48 +0100

    setting up repository

---
+++ code/search-engine.py
@@ -0,0 +1,13 @@
+# implement a search engine (embeddings)
+#   accepts a collection of docs (input)
+#   provides API to search over this collection by text query (fast api?)(end product)
+
+# get a pretrained model from huggingface -calc vector representations
+#   vector storage + retrival -> some library
+#   do an example on any source (report)
+
+# 1.import a pretrained model
+# 2. make it accept input = multiple documents
+# 3. make it generate embdeddings for the documents
+# 4. make it search with a text query
+# 4. build api around it
